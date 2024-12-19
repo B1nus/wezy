325e08bfa5035677a78ceb01790309e389f0756f
B1nus <123@1>
2024-12-19 12:57:37 +0100

    Working parser of assignments

--- parser.zig
+++ parser.zig
@@ -1,256 +1,279 @@
 pub const std = @import("std");
-pub const tokenizer = @import("tokenizer.zig");
-pub const Token = tokenizer.Token;
-pub const SourceIndex = tokenizer.SourceIndex;
 
-pub const TokenIndex = u32;
-pub const IntegerSize = u32;
-pub const StatementIndex = u32;
-pub const ExpressionIndex = u32;
+pub const Tokenizer = struct {
+    source: [:0]const u8,
+    offset: SourceIndex,
+    current: Token,
+    peek: Token,
 
-// TODO: Error handling.
+    pub const SourceIndex = usize;
 
-pub fn parse(source: [:0]const u8, allocator: std.mem.Allocator) !Parsed {
-    var offset: SourceIndex = 0;
-    var functions = std.StringHashMap(FunctionDeclaration).init(allocator);
-    var statements = std.ArrayList(Statement).init(allocator);
-    var expressions = std.ArrayList(Expression).init(allocator);
-    var indent_stack = std.ArrayList(SourceIndex).init(allocator);
-    defer indent_stack.deinit();
-    try indent_stack.append(0);
+    pub const Token = struct {
+        pos: SourceIndex,
+        tag: Tag,
 
-    var tokens = std.ArrayList(Token).init(allocator);
-    defer tokens.deinit();
+        pub const Tag = enum {
+            identifier,
+            equal,
+            integer,
+            plus,
+            newline,
+            eof,
+        };
+    };
 
-    while (tokens.items.len == 0 or tokens.getLast().tag != .eof) {
-        tokens.clearRetainingCapacity();
-        try tokenizer.next_token_line(source, offset, &indent_stack, &tokens);
-        offset = tokens.getLast().end + 1;
-        switch (tokens.items[0].tag) {
-            .keyword_integer_type => {
-                const identifier = tokens.items[1].literal(source);
-                var parameters = std.StringHashMap(IntegerSize).init(allocator);
+    pub fn init(source: [:0]const u8) Tokenizer {
+        var self = Tokenizer{ .source = source, .offset = 0, .current = undefined, .peek = undefined };
 
-                for (0..(tokens.items.len - 3) / 3) |i| {
-                    const int_size = std.fmt.parseInt(IntegerSize, source[tokens.items[3 + i * 3].pos + 1..tokens.items[3 + i * 3].end + 1], 10) catch { unreachable; };
-                    try parameters.put(source[tokens.items[4 + i * 3].pos..tokens.items[4 + i * 3].end + 1], int_size);
+        // Fill current and peek with the first two tokens
+        self.current = self.next_token();
+        self.peek = self.next_token();
+
+        return self;
+    }
+
+    pub fn next_token(self: *Tokenizer) Token {
+        const State = enum {
+            start,
+            integer,
+            identifier,
+        };
+
+        var token = Token{ .pos = self.offset, .tag = undefined };
+        state: switch (State.start) {
+            .start => {
+                switch (self.source[self.offset]) {
+                    0 => return Token{ .pos = self.source.len, .tag = .eof },
+                    '=' => token.tag = .equal,
+                    '+' => token.tag = .plus,
+                    '\n' => token.tag = .newline,
+                    'a'...'z' => continue :state .identifier,
+                    '0'...'9' => continue :state .integer,
+                    ' ', '\t' => {
+                        token.pos += 1;
+                        self.offset += 1;
+                        continue :state .start;
+                    },
+                    else => unreachable,
                 }
 
-                var functions_statements = std.ArrayList(Statement).init(allocator);
-                tokens.clearRetainingCapacity();
-                try tokenizer.next_token_line(source, offset, &indent_stack, &tokens);
-                tokens.orderedRemove(0);
-                while (tokenizer.next_token(source, offset) != .eof and tokenizer.next_token(source, offset) != .dedentation) {
-                    switch (tokens.items[0].tag) {
-                        .keyword_return => {
-                            try statements.append(Statement { .return_statement = try parse_expression(source, tokens[1..], null, &expressions) });
-                        },
-                        else => unreachable,
-                    }
-
-                    tokens.clearRetainingCapacity();
-                    try tokenizer.next_token_line(source, offset, &indent_stack, &tokens);
-                    offset = tokens.getLast().end + 1;
+                // For single character tokens, we need to move offset past them.
+                self.offset += 1;
+            },
+            .integer => {
+                self.offset += 1;
+                switch (self.source[self.offset]) {
+                    '0'...'9' => continue :state .integer,
+                    else => token.tag = .integer,
                 }
-
-                if (tokenizer.next_token(source, offset).tag == .dedentation) {
-                    offset += 1;
-                }
-
-
-
             },
             .identifier => {
-                const identifier = source[token.items[0].pos..tokens.items[0].end + 1];
+                self.offset += 1;
+                switch (self.source[self.offset]) {
+                    'a'...'z' => continue :state .identifier,
+                    else => token.tag = .identifier,
+                }
+            },
+        }
+
+        return token;
+    }
+
+    // The source code for a token. for an identifier, this is the identifier as a string.
+    pub fn token_source(self: Tokenizer, token: Token) []const u8 {
+        switch (token.tag) {
+            .integer => {
+                var end: SourceIndex = token.pos + 1;
+                while (self.source[end] >= '0' and self.source[end] <= '9') {
+                    end += 1;
+                }
+                return self.source[token.pos..end];
+            },
+            .identifier => {
+                var end: SourceIndex = token.pos + 1;
+                while (self.source[end] >= 'a' and self.source[end] <= 'z') {
+                    end += 1;
+                }
+                return self.source[token.pos..end];
             },
             else => unreachable,
         }
-        try functions.put(undefined, undefined);
-        try statements.append(undefined);
-        try expressions.append(undefined);
     }
+};
 
-    return Parsed{
-        .functions = functions,
-        .statements = statements,
-        .expressions = expressions,
+pub const Parser = struct {
+    tokenizer: *Tokenizer,
+    assignments: std.ArrayList(Assignment),
+    expressions: std.ArrayList(Expression),
+
+    pub const ExpressionIndex = usize;
+
+    pub const Precedence = enum {
+        zero,
+        sum,
     };
-}
 
-pub const LineParser = struct {
-    source: [:0]const u8,
-    token_line: []Token,
-    offset: TokenIndex,
-    expressions: *std.ArrayList(Expression),
+    pub const Token = Tokenizer.Token;
 
-    pub fn init(source: [:0]const u8, token_line: []Token, offset: TokenIndex, expressions: *std.ArrayList(Expression)) LineParser {
-        return LineParser {
-            .source = source,
-            .token_line = token_line,
-            .offset = offset,
-            .expressions = expressions,
+    pub const Assignment = struct {
+        identifier: []const u8,
+        expression: Expression,
+    };
+
+    pub const Expression = union(enum) {
+        integer: []const u8,
+        addition: [2]ExpressionIndex,
+        identifier: []const u8,
+    };
+
+    pub fn init(tokenizer: *Tokenizer, allocator: std.mem.Allocator) Parser {
+        return Parser{
+            .tokenizer = tokenizer,
+            .assignments = std.ArrayList(Assignment).init(allocator),
+            .expressions = std.ArrayList(Expression).init(allocator),
         };
     }
 
-    // Add more functions here later. maybe. but not now. Just get the tests passing first.
-
-    // pratt parsing for the expression starting at offset
-    pub fn parse_expression(self: *LineParser, offset: TokenIndex) !Expression {
-        self.offset = offset;
-        self.parse_prefix();
+    pub fn deinit(self: Parser) void {
+        self.assignments.deinit();
+        self.expressions.deinit();
     }
 
-    pub fn parse_prefix(self: *LineParser) !Expression {
-        switch (self.current_token().tag) {
-            .integer_literal => {
-                return Expression {.integer_literal = self.current_token().literal(self.source)};
-            },
-            .minus => {
-                self.offset += 1;
-                try self.expressions.append(try self.parse_prefix());
-                const rhs_index = self.expressions.items.len - 1;
-                return Expression {.negation = rhs_index};
-            },
-            .plus => {
-                self.offset += 1;
-                return self.parse_prefix();
+    pub fn parse_program(self: *Parser) void {
+        while (self.tokenizer.current.tag != .eof) {
+            self.assignments.append(self.parse_assignment()) catch unreachable;
+        }
+    }
+
+    pub fn parse_assignment(self: *Parser) Assignment {
+        while (self.tokenizer.current.tag != .identifier) {
+            self.advance();
+        }
+        const identifier = self.tokenizer.token_source(self.tokenizer.current);
+        self.advance();
+        self.advance_if(Token.Tag.equal);
+        const expression = self.parse_expression(Precedence.zero);
+        return Assignment{ .identifier = identifier, .expression = expression };
+    }
+
+    pub fn parse_expression(self: *Parser, precedence: Precedence) Expression {
+        var lhs = self.parse_prefix();
+        self.advance();
+
+        while (!tag_is_end_of_line(self.tokenizer.peek.tag) and @intFromEnum(precedence) < @intFromEnum(get_precedence(self.tokenizer.current.tag))) {
+            lhs = self.parse_infix(lhs);
+        }
+
+        return lhs;
+    }
+
+    pub fn tag_is_end_of_line(token_tag: Token.Tag) bool {
+        return token_tag == .newline or token_tag == .eof;
+    }
+
+    pub fn parse_prefix(self: *Parser) Expression {
+        switch (self.tokenizer.current.tag) {
+            .integer => {
+                return Expression{ .integer = self.tokenizer.token_source(self.tokenizer.current) };
             },
             .identifier => {
-                const identifier = self.current_token().literal(self.source);
-                self.offset += 1;
-                switch (self.current_token().tag) {
-                    .lparen => {
-                        var expressions = std.ArrayList(ExpressionIndex).init(self.expressions.allocator);
-                        var expression = try self.parse_expression(self.offset + 1);
-                        while (self.current_token().tag != .rparen and self.offset < self.token_line.len - 1) {
-                            try self.expressions.append(expression);
-                            try expressions.append(self.expressions.items.len - 1);
-                            expression = try self.parse_expression(self.offset + 1);
-                        }
-                        return Expression {.call = .{identifier, expressions}};
-                    },
-                    .dot => unreachable,
-                    else => return Expression { .identifier = identifier },
-                }
-            },
-            .lparen => {
-                self.offset += 1;
-                const exp = self.parse_expression();
-                if (self.token_line[self.offset].tag == .rparen) {
-                    return exp;
-                } else {
-                    unreachable;
-                }
+                return Expression{ .identifier = self.tokenizer.token_source(self.tokenizer.current) };
             },
             else => unreachable,
         }
     }
 
-    pub fn parse_infix(self: *LineParser, left: Expression) !Expression {
-        switch (self.current_token().tag) {
-            .rparen, .comma => {
-                return left;
-            },
+    pub fn parse_infix(self: *Parser, lhs: Expression) Expression {
+        switch (self.tokenizer.current.tag) {
             .plus => {
+                self.advance();
+                const rhs = self.parse_expression(get_precedence(self.tokenizer.current.tag));
+                const lhs_index = self.append_expression(lhs);
+                const rhs_index = self.append_expression(rhs);
+                return Expression{ .addition = .{ lhs_index, rhs_index } };
             },
-            .minus => {
-            },
-            .dot => unreachable, // method call?
+            else => unreachable,
         }
     }
 
-    pub fn current_token(self: LineParser) Token {
-        return self.token_line[self.offset];
+    pub inline fn get_precedence(token_tag: Token.Tag) Precedence {
+        return switch (token_tag) {
+            .plus => Precedence.sum,
+            .integer => Precedence.zero,
+            else => unreachable,
+        };
     }
 
-    pub fn next_token(self: LineParser) Token {
-        self.offset += 1;
-        return self.current_token();
+    pub fn append_expression(self: *Parser, expression: Expression) ExpressionIndex {
+        self.expressions.append(expression) catch unreachable;
+        return self.expressions.items.len - 1;
     }
-};
 
-pub const Parsed = struct {
-    functions: std.StringHashMap(FunctionDeclaration),
-    statements: std.ArrayList(Statement),
-    expressions: std.ArrayList(Expression),
+    pub fn advance(self: *Parser) void {
+        self.tokenizer.current = self.tokenizer.peek;
+        self.tokenizer.peek = self.tokenizer.next_token();
+    }
 
-    pub fn deinit(self: @This()) void {
-        var func_it = self.functions.valueIterator();
-        for (func_it.next()) |func| {
-            func.parameters.deinit();
-            func.statements.deinit();
+    pub fn advance_if(self: *Parser, token_tag: Token.Tag) void {
+        if (token_tag == self.tokenizer.current.tag) {
+            self.advance();
+        } else {
+            unreachable;
         }
-        self.functions.deinit();
-        self.statements.deinit();
     }
-};
-
-pub const FunctionDeclaration = struct {
-    parameters: std.StringHashMap(IntegerSize),
-    statements: std.ArrayList(Statement),
-};
-
-pub const Statement = union(enum) {
-    return_statement: Expression,
-    assignment: struct { []const u8, Expression },
-};
-
-pub const Expression = union(enum) {
-    integer_literal: []const u8,
-    negation: ExpressionIndex,
-    addition: [2]ExpressionIndex,
-    identifier: []const u8,
-    call: struct { []const u8, std.ArrayList(ExpressionIndex) },
 };
 
 pub const expect = std.testing.expect;
+test "assign tokens" {
+    // The indentation is encoded here. The first tokens position is the indentation
+    // of the new line. It does not get easier than this.
+    const source = "z= 5 +4  ";
+    var tokenizer = Tokenizer.init(source);
+    try expect(tokenizer.current.tag == .identifier);
+    try expect(tokenizer.current.pos == 0);
+    try expect(tokenizer.peek.tag == .equal);
+    try expect(tokenizer.peek.pos == 1);
+    const token_5 = tokenizer.next_token();
+    const token_plus = tokenizer.next_token();
+    const token_4 = tokenizer.next_token();
+    const token_eof = tokenizer.next_token();
+    try expect(token_5.tag == .integer);
+    try expect(token_5.pos == 3);
+    try expect(token_plus.tag == .plus);
+    try expect(token_plus.pos == 5);
+    try expect(token_4.tag == .integer);
+    try expect(token_4.pos == 6);
+    try expect(token_eof.tag == .eof);
+    try expect(token_eof.pos == 9);
+}
 
-test "Simple Add function" {
+test "assign parsing" {
+    const source = "z = 5 + 4";
+    var tokenizer = Tokenizer.init(source);
+    var parser = Parser.init(&tokenizer, std.testing.allocator);
+    defer parser.deinit();
+    const assignment = parser.parse_assignment();
+    try expect(std.mem.eql(u8, assignment.identifier, "z"));
+    try expect(std.mem.eql(u8, parser.expressions.items[assignment.expression.addition[0]].integer, "5"));
+    try expect(std.mem.eql(u8, parser.expressions.items[assignment.expression.addition[1]].integer, "4"));
+}
+
+test "many assign expressions" {
     const source =
-        \\i32 add(i32 a, i32 b)
-        \\  return a + b
+        \\z = 1 + 1
         \\
-        \\z = add(8, - 5)
+        \\x = z + 2
     ;
-    const parsed = try parse(source, std.testing.allocator);
-    defer parsed.deinit();
+    var tokenizer = Tokenizer.init(source);
+    var parser = Parser.init(&tokenizer, std.testing.allocator);
+    defer parser.deinit();
 
-    try expect(parsed.statements.items.len == 1);
-    try expect(parsed.expressions.items.len == 7);
-    try expect(parsed.statements.items[0] == Statement.assignment);
-    const assign_ident, const assign_expr = parsed.statements.items[0].assignment;
-    // Call Expression
-    try expect(std.mem.eql(u8, assign_ident, "z"));
-    try expect(assign_expr == Expression.call);
-    const call_ident, const call_expressions = assign_expr.call;
-    try expect(std.mem.eql(u8, call_ident, "add"));
-    try expect(call_expressions.items.len == 2);
-    const call_arg1 = parsed.expressions.items[call_expressions.items[0]];
-    const call_arg2 = parsed.expressions.items[call_expressions.items[1]];
-    try expect(call_expressions.items[0] == 2);
-    try expect(call_arg1 == .integer_literal);
-    try expect(call_arg2 == .negation);
-    const call_int2 = parsed.expressions.items[call_arg2.negation].integer_literal;
-    try expect(std.mem.eql(u8, call_arg1.integer_literal, "8"));
-    try expect(std.mem.eql(u8, call_int2, "8"));
+    parser.parse_program();
+    try expect(std.mem.eql(u8, parser.assignments.items[0].identifier, "z"));
+    try expect(std.mem.eql(u8, parser.expressions.items[parser.assignments.items[0].expression.addition[0]].integer, "1"));
+    try expect(std.mem.eql(u8, parser.expressions.items[parser.assignments.items[0].expression.addition[1]].integer, "1"));
 
-    // Function Declaration
-    try expect(parsed.functions.count() == 1);
-    try expect(parsed.functions.get("add") != null);
-    try expect(parsed.functions.get("add").?.parameters.count() == 2);
-    try expect(parsed.functions.get("add").?.parameters.get("a") != null);
-    try expect(parsed.functions.get("add").?.parameters.get("b") != null);
-    try expect(parsed.functions.get("add").?.parameters.get("a").? == 32);
-    try expect(parsed.functions.get("add").?.parameters.get("b").? == 32);
-    try expect(parsed.functions.get("add").?.statements.items.len == 1);
-    try expect(parsed.functions.get("add").?.statements.items[0] == Statement.return_statement);
-    // Function Statement
-    const function_statement = parsed.functions.get("add").?.statements.items[0].return_statement;
-    try expect(function_statement == Expression.addition);
-    try expect(parsed.expressions.items[function_statement.addition[0]] == Expression.identifier);
-    try expect(parsed.expressions.items[function_statement.addition[1]] == Expression.identifier);
-    const a = parsed.expressions.items[function_statement.addition[0]].identifier;
-    const b = parsed.expressions.items[function_statement.addition[1]].identifier;
-    try expect(std.mem.eql(u8, a, "a"));
-    try expect(std.mem.eql(u8, b, "b"));
+    try expect(std.mem.eql(u8, parser.assignments.items[1].identifier, "x"));
+    try expect(std.mem.eql(u8, parser.expressions.items[parser.assignments.items[1].expression.addition[0]].identifier, "z"));
+    try expect(std.mem.eql(u8, parser.expressions.items[parser.assignments.items[1].expression.addition[1]].integer, "2"));
 }
