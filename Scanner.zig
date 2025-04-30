677f06df2936cee73a7ceda0202b88c7be81525b
B1nus <119787571+B1nus@users.noreply.github.com>
2025-04-30 13:15:43 +0200

    Scanner

---
+++ Scanner.zig
@@ -0,0 +1,245 @@
+source: []const u8,
+word: ?Word,
+start: usize,
+index: usize,
+
+pub fn init(source: []const u8) @This() {
+    return .{
+        .source = source,
+        .word = null,
+        .start = 0,
+        .index = 0,
+    };
+}
+
+const Word = union(enum) {
+    newline: usize,
+    doubleNewline: usize,
+    string: []const u8,
+    word: []const u8,
+};
+
+fn getByte(self: @This()) u8 {
+    return if (self.index < self.source.len) self.source[self.index] else 0;
+}
+
+fn nextByte(self: *@This()) u8 {
+    self.index += 1;
+    return self.getByte();
+}
+
+fn getPreviousByte(self: @This()) u8 {
+    return if (self.index > 0) self.source[self.index - 1] else 0;
+}
+
+pub fn previousWord(self: *@This()) !Word {
+    if (self.index == 0) {
+        return error.BeginningOfFile;
+    }
+    switch (self.getByte()) {
+        0 => {
+            self.skipBackward(" \n");
+            if (self.getByte() == ' ') {
+                self.index += 1;
+            }
+        },
+        '\n' => {
+            self.index -= 1;
+            self.skipBackward(" \n");
+            if (self.getByte() == ' ') {
+                self.index += 1;
+            }
+        },
+        else => {
+            self.index -= 1;
+            if (self.getPreviousByte() == ' ') {
+                self.skipBackwardWhile(" \n");
+                self.index += 1;
+            } else {
+                self.index -= 1;
+                self.skipBackward(" \n");
+                if (self.index != 0) {
+                    self.index += 1;
+                }
+            }
+        },
+    }
+    const mem_pos = self.index;
+    const word = self.nextWord();
+    self.index = mem_pos;
+    return word;
+}
+
+pub fn readUntil(self: *@This(), end: []const u8) []const u8 {
+    self.start = self.index;
+    while (self.nextByte() != 0 and std.mem.count(u8, end, &.{ self.getByte() }) == 0) { }
+    return self.source[self.start..self.index];
+}
+
+fn skipBackward(self: *@This(), end: []const u8) void {
+    while (self.index > 0 and std.mem.count(u8, end, &.{ self.getByte() }) == 0) {  self.index -= 1; }
+}
+
+fn skipBackwardWhile(self: *@This(), bytes: []const u8) void {
+    while (self.index > 0 and std.mem.count(u8, bytes, &.{ self.getByte() }) > 0) {  self.index -= 1; }
+}
+
+pub fn nextWord(self: *@This()) !Word {
+    self.start = self.index;
+    switch (self.getByte()) {
+        0 => switch (self.getPreviousByte()) {
+            '\n' => return error.TrailingNewline,
+            ' ' => return error.TrailingSpace,
+            else => return error.EndOfFile,
+        },
+        ' ' => return error.UnexpectedSpace,
+        '\n' => switch (self.nextByte()) {
+            0 => return error.TrailingNewline,
+            '\n' => switch (self.nextByte()) {
+                0 => return error.TrailingNewline,
+                '\n' => return error.TooManyNewline,
+                else => {
+                    const n = self.count(' ');
+                    if (self.getByte() == '\n') {
+                        return error.UnusedIndentation;
+                    } else {
+                        return .{ .doubleNewline = n };
+                    }
+                },
+            },
+            else => {
+                const n = self.count(' ');
+                if (self.getByte() == '\n') {
+                    return error.UnusedIndentation;
+                } else {
+                    return .{ .newline = n };
+                }
+            },
+        },
+        else => {
+            const word = self.readUntil(" \n");
+            if (self.getByte() == ' ') {
+                _ = self.nextByte();
+            }
+            return .{ .word = word };
+        },
+    }
+}
+
+fn count(self: *@This(), byte: u8) usize {
+    if (self.getByte() == byte) {
+        var n: usize = 1;
+        while (self.nextByte() == byte) {
+            n += 1;
+        }
+        return n;
+    } else {
+        return 0;
+    }
+}
+
+const std = @import("std");
+const expect = std.testing.expect;
+
+test "empty" {
+    const source = "";
+    var scanner = @This().init(source);
+    try expect(scanner.nextWord() == error.EndOfFile);
+}
+
+test "only space" {
+    const source = " ";
+    var scanner = @This().init(source);
+    try expect(scanner.nextWord() == error.UnexpectedSpace);
+}
+
+test "one word" {
+    const source = "hello";
+    var scanner = @This().init(source);
+    try expect(std.mem.eql(u8, (try scanner.nextWord()).word, "hello"));
+}
+
+test "trailing space at end" {
+    const source = "hello ";
+    var scanner = @This().init(source);
+    _ = try scanner.nextWord();
+    try expect(scanner.nextWord() == error.TrailingSpace);
+}
+
+test "trailing newline at end" {
+    const source = "hello \n";
+    var scanner = @This().init(source);
+    _ = try scanner.nextWord();
+    try expect(scanner.nextWord() == error.TrailingNewline);
+}
+
+test "double space" {
+    const source = "hello   my friend";
+    var scanner = @This().init(source);
+    _ = try scanner.nextWord();
+    try expect(scanner.nextWord() == error.UnexpectedSpace);
+}
+
+test "read rest of line" {
+    const source = "hello   my friend";
+    var scanner = @This().init(source);
+    _ = try scanner.nextWord();
+    try expect(std.mem.eql(u8, scanner.readUntil("\n"), "  my friend"));
+}
+
+test "newline" {
+    const source = "\n  ";
+    var scanner = @This().init(source);
+    try expect((try scanner.nextWord()).newline == 2);
+}
+
+test "double newline" {
+    const source = "\n\n    ";
+    var scanner = @This().init(source);
+    try expect((try scanner.nextWord()).doubleNewline == 4);
+}
+
+test "tripple newline" {
+    const source = "\n\n\n    ";
+    var scanner = @This().init(source);
+    try expect(scanner.nextWord() == error.TooManyNewline);
+}
+
+test "unused indent" {
+    const source = "\n  \n";
+    var scanner = @This().init(source);
+    try expect(scanner.nextWord() == error.UnusedIndentation);
+}
+
+test "previous word" {
+    const source = "hello world\n\n  hello";
+    var scanner = @This().init(source);
+    const first_word = try scanner.nextWord();
+    const second_word = try scanner.nextWord();
+    const third_word = try scanner.nextWord();
+    const fourth_word = try scanner.nextWord();
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, fourth_word.word));
+    try expect((try scanner.previousWord()).doubleNewline == third_word.doubleNewline);
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, second_word.word));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, first_word.word));
+}
+
+test "func result thingie" {
+    const source = "func main param x i32 y i32 result i32 result i32 i32 i32";
+    var scanner = @This().init(source);
+    _ = scanner.readUntil("\n");
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "result"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "result"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "y"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "x"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "param"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "main"));
+    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "func"));
+    try expect(scanner.previousWord() == error.BeginningOfFile);
+}
--- compiler.zig
+++ compiler.zig
@@ -6,7 +6,6 @@ const Declaration = enum {
     import,
     @"export",
     memory,
-    @"type",
     table,
     elem,
     global,
@@ -14,14 +13,14 @@ const Declaration = enum {
     func,
 };
 
-const ValType = enum(u8) {
-    @"i32" = 0x7f,
-    @"i64" = 0x7e,
-    @"f32" = 0x7d,
-    @"f64" = 0x7c,
-    @"v128" = 0x7b,
-    @"funcref" = 0x70,
-    @"externref" = 0x6f,
+const Valtype = enum(u8) {
+    i32 = 0x7f,
+    i64 = 0x7e,
+    f32 = 0x7d,
+    f64 = 0x7c,
+    v128 = 0x7b,
+    funcref = 0x70,
+    externref = 0x6f,
 };
 
 const ImportType = enum {
@@ -45,26 +44,39 @@ const CompilerError = error{
     TooManySpaces,
     DeclarationIsTooLate,
     DeclarationCannotChange,
-    ExpectedEndOfDeclaration,
+    ExpectedEndOfLine,
     TooManyNewlines,
     ExpectedKeywordParamOrResult,
-    UnknownValType,
-    ExepctedAParameterType,
+    ExpectedKeywordResult,
+    UnknownValtype,
+    ExpectedAParamType,
     ExpectedAResultType,
     SpaceAtTheEndOfALine,
+    MissingExportedName,
+    MissingExportWithKeyword,
+    ExpectedExportWithKeyword,
+    MissingExportType,
+    MissingExportName,
+    UnknownExportType,
+    MissingMemoryName,
+    MissingMinPageCount,
+    ExpectedMinPageCountToBePositiveInteger,
+    ExpectedMaxPageCountToBePositiveInteger,
 };
 
 pub fn nextWord(source: []u8, index: *usize, target: u8, banned: ?u8) ![]u8 {
     const startIndex = index.*;
-    if (banned != null and source[index.*] == banned) {
+    if (source[index.*] == banned) {
         return error.BannedChar;
     }
-    while (index.* < source.len and source[index.*] != target) : (index.* += 1) { }
+    while (index.* < source.len and source[index.*] != target and source[index.*] != banned) : (index.* += 1) {}
     const word = source[startIndex..index.*];
 
     switch (skip(source, index, target)) {
-        .One, .EndOfFile, => return word,
-        .No => unreachable,
+        .No,
+        .One,
+        .EndOfFile,
+        => return word,
         else => return error.TooManySeparators,
     }
 }
@@ -91,20 +103,82 @@ pub fn skip(source: []u8, index: *usize, target: u8) Skip {
     };
 }
 
-pub fn parseFuncType(_: []u8, _: *usize, typeWriter: anytype) !void {
+pub fn parseFuncType(source: []u8, index: *usize, typeWriter: anytype) !void {
     try typeWriter.writeByte(0x60);
-    try typeWriter.writeByte(0);
-    try typeWriter.writeByte(0);
+    if (index.* >= source.len or source[index.*] == '\n') {} else {
+        const word = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+            error.TooManySeparators => return CompilerError.TooManySpaces,
+            error.BannedChar => {
+                try typeWriter.writeByte(0);
+                try typeWriter.writeByte(0);
+                return;
+            },
+        };
+        if (std.mem.eql(u8, "param", word)) {
+            var paramBuf = try std.BoundedArray(u8, 0x10).init(0);
+            if (parseTypeList(source, index, paramBuf.writer()) catch |err| switch (err) {
+                error.ExpectedValtype => return CompilerError.ExpectedAParamType,
+                else => return err,
+            }) |word2| {
+                try std.leb.writeUleb128(typeWriter, paramBuf.len);
+                try typeWriter.writeAll(paramBuf.slice());
+                if (std.mem.eql(u8, "result", word2)) {
+                    var resultBuf = try std.BoundedArray(u8, 0x10).init(0);
+                    if (parseTypeList(source, index, resultBuf.writer()) catch |err| switch (err) {
+                        error.ExpectedValtype => return CompilerError.ExpectedAResultType,
+                        else => return err,
+                    }) |_| {
+                        return CompilerError.ExpectedEndOfLine;
+                    } else {
+                        try std.leb.writeUleb128(typeWriter, resultBuf.len);
+                        try typeWriter.writeAll(resultBuf.slice());
+                    }
+                } else {
+                    return CompilerError.ExpectedKeywordResult;
+                }
+            }
+        } else if (std.mem.eql(u8, "result", word)) {
+            var resultBuf = try std.BoundedArray(u8, 0x10).init(0);
+            if (parseTypeList(source, index, resultBuf.writer()) catch |err| switch (err) {
+                error.ExpectedValtype => return CompilerError.ExpectedAResultType,
+                else => return err,
+            }) |_| {
+                return CompilerError.ExpectedEndOfLine;
+            } else {
+                try std.leb.writeUleb128(typeWriter, resultBuf.len);
+                try typeWriter.writeAll(resultBuf.slice());
+            }
+        } else {
+            return CompilerError.ExpectedKeywordParamOrResult;
+        }
+    }
 }
 
-pub fn findOrAppendType(types: anytype, @"type": []u8) !usize {
-    for (types.slice(), 0..) |curType, index| {
-        if (std.mem.eql(u8, curType, @"type")) {
+pub fn parseTypeList(source: []u8, index: *usize, writer: anytype) !?[]u8 {
+    var word = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+        error.TooManySeparators => return CompilerError.TooManySpaces,
+        error.BannedChar => {
+            return error.ExpectedValtype;
+        },
+    };
+    while (std.meta.stringToEnum(Valtype, word)) |valtype| {
+        try writer.writeByte(@intFromEnum(valtype));
+        word = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+            error.TooManySeparators => return CompilerError.TooManySpaces,
+            error.BannedChar => return null,
+        };
+    }
+    return word;
+}
+
+pub fn findOrAppend(list: anytype, item: []u8) !usize {
+    for (list.slice(), 0..) |cur, index| {
+        if (std.mem.eql(u8, cur, item)) {
             return index;
         }
     }
-    try types.append(@"type");
-    return types.len - 1;
+    try list.append(item);
+    return list.len - 1;
 }
 
 pub fn writeSection(writer: anytype, count: usize, content: []u8, sectionCode: u8) !void {
@@ -123,10 +197,26 @@ pub fn compileCoreModule(source: []u8, index: *usize, writer: anytype) !void {
     var maxDeclaration: usize = 12;
 
     var types = try std.BoundedArray([]u8, 0x100).init(0);
+    var funcs = try std.BoundedArray([]u8, 0x100).init(0);
     var imports = try std.BoundedArray([]u8, 0x100).init(0);
     var import = try std.BoundedArray(u8, 0x100).init(0);
+    var exports: usize = 0;
+    var @"export" = try std.BoundedArray(u8, 0x100).init(0);
+    var memory = try std.BoundedArray(u8, 0x100).init(0);
+    var memories = try std.BoundedArray([]u8, 0x100).init(0);
 
-    while (index.* < source.len) : (index.* += 1) {
+    while (index.* < source.len) {
+        const startIndex = index.*;
+        if (source.len > index.* + 2 and std.mem.eql(u8, source[index.*..index.*+2], "//")) {
+            if (std.mem.indexOfScalarPos(u8, source, index.*, '\n')) |i| {
+                index.* = i;
+                _ = skip(source, index, ' ');
+                _ = skip(source, index, '\n');
+                continue;
+            } else {
+                break;
+            }
+        }
         const declarationSlice = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
             error.TooManySeparators => return CompilerError.TooManySpaces,
             error.BannedChar => return CompilerError.MissingDeclaration,
@@ -171,11 +261,13 @@ pub fn compileCoreModule(source: []u8, index: *usize, writer: anytype) !void {
                         try import.writer().writeAll(itemName);
                         try import.writer().writeByte(@intCast(@intFromEnum(@"type")));
 
+                        _ = try findOrAppend(&funcs, name);
+
                         switch (@"type") {
                             .func => {
                                 var typeBuf = try std.BoundedArray(u8, 0x10).init(0);
                                 try parseFuncType(source, index, typeBuf.writer());
-                                const typeIdx = try findOrAppendType(&types, typeBuf.slice());
+                                const typeIdx = try findOrAppend(&types, typeBuf.slice());
                                 try std.leb.writeUleb128(import.writer(), typeIdx);
                             },
                             else => unreachable,
@@ -185,15 +277,85 @@ pub fn compileCoreModule(source: []u8, index: *usize, writer: anytype) !void {
                         return CompilerError.UnknownImportType;
                     }
                 },
+                .@"export" => {
+                    const exportedName = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                        error.TooManySeparators => return CompilerError.TooManySpaces,
+                        error.BannedChar => return CompilerError.MissingExportedName,
+                    };
+                    const with = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                        error.TooManySeparators => return CompilerError.TooManySpaces,
+                        error.BannedChar => return CompilerError.MissingExportWithKeyword,
+                    };
+                    if (!std.mem.eql(u8, "with", with)) {
+                        return CompilerError.ExpectedExportWithKeyword;
+                    }
+                    const typeStr = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                        error.TooManySeparators => return CompilerError.TooManySpaces,
+                        error.BannedChar => return CompilerError.MissingExportType,
+                    };
+                    if (std.meta.stringToEnum(ImportType, typeStr)) |@"type"| {
+                        const name = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                            error.TooManySeparators => return CompilerError.TooManySpaces,
+                            error.BannedChar => return CompilerError.MissingExportName,
+                        };
+                        try std.leb.writeUleb128(@"export".writer(), exportedName.len);
+                        try @"export".writer().writeAll(exportedName);
+
+                        const idx = switch (@"type") {
+                            .func => try findOrAppend(&funcs, name),
+                            .memory => try findOrAppend(&memories, name),
+                            else => unreachable,
+                        };
+                        try @"export".writer().writeByte(@intCast(@intFromEnum(@"type")));
+                        try std.leb.writeUleb128(@"export".writer(), idx);
+                        exports += 1;
+                    } else {
+                        return CompilerError.UnknownExportType;
+                    }
+                },
+                .memory => {
+                    const name = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                        error.TooManySeparators => return CompilerError.TooManySpaces,
+                        error.BannedChar => return CompilerError.MissingMemoryName,
+                    };
+
+                    const minStr = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                        error.TooManySeparators => return CompilerError.TooManySpaces,
+                        error.BannedChar => return CompilerError.MissingMinPageCount,
+                    };
+
+                    const min = std.fmt.parseInt(usize, minStr, 10) catch {
+                        return CompilerError.ExpectedMinPageCountToBePositiveInteger;
+                    };
+
+                    const maxStr: ?[]u8 = nextWord(source, index, ' ', '\n') catch |err| switch (err) {
+                        error.TooManySeparators => return CompilerError.TooManySpaces,
+                        error.BannedChar => null,
+                    };
+
+                    if (maxStr) |maxStr_| {
+                        const max = std.fmt.parseInt(usize, maxStr_, 10) catch {
+                            return CompilerError.ExpectedMaxPageCountToBePositiveInteger;
+                        };
+
+                        try memory.append(1);
+                        try std.leb.writeUleb128(memory.writer(), min);
+                        try std.leb.writeUleb128(memory.writer(), max);
+                    } else {
+                        try memory.append(0);
+                        try std.leb.writeUleb128(memory.writer(), min);
+                    }
+                    _ = try findOrAppend(&memories, name);
+                },
                 else => unreachable,
             }
 
-            switch(skip(source, index, ' ')) {
+            switch (skip(source, index, ' ')) {
                 .No, .EndOfFile => {},
                 else => return CompilerError.SpaceAtTheEndOfALine,
             }
-            switch(skip(source, index, '\n')) {
-                .No => return CompilerError.ExpectedEndOfDeclaration,
+            switch (skip(source, index, '\n')) {
+                .No => return CompilerError.ExpectedEndOfLine,
                 .One => maxDeclaration = minDeclaration,
                 .Two, .EndOfFile => {
                     minDeclaration += 1;
@@ -202,6 +364,7 @@ pub fn compileCoreModule(source: []u8, index: *usize, writer: anytype) !void {
                 .TooMany => return CompilerError.TooManyNewlines,
             }
         } else {
+            index.* = startIndex;
             return CompilerError.UnknownDeclaration;
         }
     }
@@ -211,6 +374,8 @@ pub fn compileCoreModule(source: []u8, index: *usize, writer: anytype) !void {
     }
     try writeSection(writer, types.len, typeSec.slice(), 1);
     try writeSection(writer, imports.len, import.slice(), 2);
+    try writeSection(writer, memories.len, memory.slice(), 5);
+    try writeSection(writer, exports, @"export".slice(), 7);
 }
 
 pub fn readFile(reader: anytype, buffer: []u8) ![]u8 {
@@ -218,6 +383,9 @@ pub fn readFile(reader: anytype, buffer: []u8) ![]u8 {
 }
 
 pub fn indexOfBackwards(haystack: []u8, needle: u8) ?usize {
+    if (haystack.len == 0) {
+        return null;
+    }
     var index: usize = haystack.len - 1;
     while (index > 0 and haystack[index] != needle) : (index -= 1) {}
     return if (index > 0) index else null;
@@ -229,20 +397,22 @@ pub fn main() !void {
 
     var index: usize = 0;
     const writer = std.io.getStdOut().writer();
-    try writer.writeAll(&.{0,'a','s','m',1,0,0,0});
+    try writer.writeAll(&.{ 0, 'a', 's', 'm', 1, 0, 0, 0 });
     const errWriter = std.io.getStdErr().writer();
     compileCoreModule(source, &index, writer) catch |err| {
-        const startIndex = indexOfBackwards(source[0..index], '\n') orelse 0;
+        const startIndex = if (indexOfBackwards(source[0..index], '\n')) |i| i + 1 else 0;
         const endIndex = std.mem.indexOfScalarPos(u8, source, index, '\n') orelse source.len;
-        const line = 1 + std.mem.count(u8, source[0..index], "\n");
-        const column = index - startIndex + 1;
-        try errWriter.print("\n\x1b[1;31m{s}\x1b[0m at line \x1b[1;37m{d}\x1b[0m column \x1b[1;37m{d}\x1b[0m\n{s}\n", .{@errorName(err), line, column, source[startIndex..endIndex]});
-        for (0..column - 1) |_| {
+        const line = std.mem.count(u8, source[0..index], "\n");
+        const column = index - startIndex;
+        try errWriter.print("\x1b[1;31m{s}\x1b[0m at line \x1b[1;37m{d}\x1b[0m column \x1b[1;37m{d}\x1b[0m\n{s}\n", .{ @errorName(err), line + 1, column + 1, source[startIndex..endIndex] });
+        for (0..column) |_| {
             try errWriter.writeByte(' ');
         }
         try errWriter.writeAll("\x1b[1;32m^");
-        for (0..endIndex - startIndex - column) |_| {
-            try errWriter.writeAll("~");
+        if (index < endIndex) {
+            for (0..endIndex - index - 1) |_| {
+                try errWriter.writeAll("~");
+            }
         }
         try errWriter.writeAll("\x1b[0m\n");
     };
