94646c387dfa2602ee37e6d555023ae1ca0e98df
B1nus <119787571+B1nus@users.noreply.github.com>
2025-05-04 00:21:14 +0200

    Better scanner. I love this code. It's beautiful

--- Scanner.zig
+++ Scanner.zig
@@ -1,268 +1,210 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const eql = std.mem.eql;
+
 source: []const u8,
-word: Word,
 start: usize,
-index: usize,
+end: usize,
+next: usize,
+line: usize,
+lineStart: usize,
+
+const ImportExportType = enum(u8) {
+    func = 0,
+    table = 1,
+    memory = 2,
+    global = 3,
+};
+
+const Valtype = enum(u8) {
+    i32 = 0x7f,
+    i64 = 0x7e,
+    f32 = 0x7d,
+    f64 = 0x7c,
+    v128 = 0x7b,
+    funcref = 0x70,
+    externref = 0x6f,
+};
+
+const Prefix = enum(u8) {
+    import,
+    @"export",
+    type,
+    memory,
+    data,
+    global,
+    table,
+    elem,
+    func,
+};
 
 pub fn init(source: []const u8) @This() {
     return .{
         .source = source,
-        .word = .beginningOfFile,
-        .start = 0,
-        .index = 0,
+        .next = 0,
+        .start = undefined,
+        .end = undefined,
+        .line = 1,
+        .lineStart = 0,
     };
 }
 
-const Word = union(enum) {
-    newline: usize,
-    doubleNewline: usize,
-    word: []const u8,
-    endOfFile,
-    beginningOfFile,
-};
+pub fn word(self: *@This()) ?[]const u8 {
+    self.start = self.next;
+    self.end = self.start;
 
-fn getByte(self: @This()) u8 {
-    return if (self.index < self.source.len) self.source[self.index] else 0;
-}
-
-fn nextByte(self: *@This()) u8 {
-    self.index += 1;
-    return self.getByte();
-}
-
-fn getPreviousByte(self: @This()) u8 {
-    return if (self.index > 0) self.source[self.index - 1] else 0;
-}
-
-pub fn previousWord(self: *@This()) !Word {
-    if (self.index == 0) {
-        self.word = .beginningOfFile;
-        return self.word;
-    }
-    switch (self.getByte()) {
-        0 => {
-            self.skipBackwardUntil(" \n");
-            if (self.getByte() == ' ') {
-                self.index += 1;
-            }
-        },
-        '\n' => {
-            self.index -= 1;
-            self.skipBackwardUntil(" \n");
-            if (self.getByte() == ' ') {
-                self.index += 1;
-            }
-        },
-        else => {
-            self.index -= 1;
-            if (self.getPreviousByte() == ' ') {
-                self.skipBackwardWhile(" \n");
-                self.index += 1;
-            } else {
-                self.index -= 1;
-                self.skipBackwardUntil(" \n");
-                if (self.index != 0) {
-                    self.index += 1;
-                }
-            }
-        },
-    }
-    const mem_pos = self.index;
-    self.word = try self.nextWord();
-    self.index = mem_pos;
-    return self.word;
-}
-
-pub fn skipUntil(self: *@This(), end: []const u8) void {
-    while (self.nextByte() != 0 and std.mem.count(u8, end, &.{self.getByte()}) == 0) {}
-}
-
-fn readUntil(self: *@This(), end: []const u8) []const u8 {
-    self.start = self.index;
-    self.skipUntil(end);
-    return self.source[self.start..self.index];
-}
-
-pub fn skipBackwardUntil(self: *@This(), end: []const u8) void {
-    while (self.index > 0 and std.mem.count(u8, end, &.{self.getByte()}) == 0) {
-        self.index -= 1;
-    }
-}
-
-fn skipBackwardWhile(self: *@This(), bytes: []const u8) void {
-    while (self.index > 0 and std.mem.count(u8, bytes, &.{self.getByte()}) > 0) {
-        self.index -= 1;
-    }
-}
-
-pub fn expectWord(self: *@This(), err: anytype) ![]const u8 {
-    switch (try self.nextWord()) {
-        .word => |word| return word,
-        else => return err,
-    }
-}
-
-pub fn nextWord(self: *@This()) !Word {
-    self.start = self.index;
-    switch (self.getByte()) {
-        0 => switch (self.getPreviousByte()) {
-            '\n' => return error.TrailingNewline,
-            ' ' => return error.TrailingSpace,
-            else => {
-                self.word = .endOfFile;
-                return self.word;
-            },
-        },
-        ' ' => return error.UnexpectedSpace,
-        '\n' => switch (self.nextByte()) {
-            0 => return error.TrailingNewline,
-            '\n' => switch (self.nextByte()) {
-                0 => return error.TrailingNewline,
-                '\n' => return error.TooManyNewline,
-                else => {
-                    const n = self.count(' ');
-                    if (self.getByte() == '\n') {
-                        return error.UnusedIndentation;
-                    } else {
-                        self.word = .{ .doubleNewline = n };
-                        return self.word;
-                    }
-                },
-            },
-            else => {
-                const n = self.count(' ');
-                if (self.getByte() == '\n') {
-                    return error.UnusedIndentation;
+    while (true) : (self.end += 1) {
+        if (self.end >= self.source.len) {
+            self.end = self.source.len;
+            self.next = if (self.end > self.start) self.source.len else return null;
+        } else {
+            switch (self.source[self.end]) {
+                '\n' => if (self.end > self.start) {
+                    self.next = self.end;
                 } else {
-                    self.word = .{ .newline = n };
-                    return self.word;
-                }
-            },
-        },
-        else => {
-            const word = self.readUntil(" \n");
-            if (self.getByte() == ' ') {
-                _ = self.nextByte();
+                    return null;
+                },
+                ' ' => if (self.end > self.start) { self.next = self.end + 1; } else {return null;},
+                else => continue,
             }
-            self.word = .{ .word = word };
-            return self.word;
-        },
-    }
-}
-
-fn count(self: *@This(), byte: u8) usize {
-    if (self.getByte() == byte) {
-        var n: usize = 1;
-        while (self.nextByte() == byte) {
-            n += 1;
         }
-        return n;
+
+        break;
+    }
+
+    return self.source[self.start..self.end];
+}
+
+pub fn string(self: *@This()) ?[]const u8 {
+    self.start = self.next;
+    self.end = self.start;
+
+    while (self.end < self.source.len and self.source[self.end] != '\n') : (self.end += 1) { }
+
+    self.next = if (self.end > self.start) self.end else return null;
+
+    return self.source[self.start..self.end];
+}
+
+pub fn newline(self: *@This()) bool {
+    if (self.next < self.source.len and self.source[self.next] == '\n') {
+        self.next += 1;
+        return true;
     } else {
-        return 0;
+        return false;
     }
 }
 
-const std = @import("std");
-const expect = std.testing.expect;
+pub fn indentation(self: *@This()) usize {
+    self.start = self.next;
+    self.end = self.start;
 
-test "empty" {
-    const source = "";
-    var scanner = @This().init(source);
-    try expect(try scanner.nextWord() == .endOfFile);
+    while (self.end < self.source.len and self.source[self.end] == ' ') : (self.end += 1) { }
+
+    self.next = self.end;
+
+    return self.end - self.start;
 }
 
-test "only space" {
-    const source = " ";
-    var scanner = @This().init(source);
-    try expect(scanner.nextWord() == error.UnexpectedSpace);
+pub fn endOfFile(self: *@This()) bool {
+    return self.next >= self.source.len;
 }
 
-test "one word" {
-    const source = "hello";
-    var scanner = @This().init(source);
-    try expect(std.mem.eql(u8, (try scanner.nextWord()).word, "hello"));
+pub fn integer(self: *@This(), T: type) ?T {
+    if (self.word()) |w| {
+        return std.fmt.parseInt(T, w, 0) catch return null;
+    } else {
+        return null;
+    }
 }
 
-test "trailing space at end" {
-    const source = "hello ";
-    var scanner = @This().init(source);
-    _ = try scanner.nextWord();
-    try expect(scanner.nextWord() == error.TrailingSpace);
+pub fn float(self: *@This(), T: type) ?T {
+    if (self.word()) |w| {
+        return std.fmt.parseFloat(T, w) catch return null;
+    } else {
+        return null;
+    }
 }
 
-test "trailing newline at end" {
-    const source = "hello \n";
-    var scanner = @This().init(source);
-    _ = try scanner.nextWord();
-    try expect(scanner.nextWord() == error.TrailingNewline);
+pub fn variant(self: *@This(), E: type) ?E {
+    if (self.word()) |w| {
+        return std.meta.stringToEnum(E, w);
+    } else {
+        return null;
+    }
 }
 
-test "double space" {
-    const source = "hello   my friend";
-    var scanner = @This().init(source);
-    _ = try scanner.nextWord();
-    try expect(scanner.nextWord() == error.UnexpectedSpace);
+pub fn valtype(self: *@This()) ?Valtype {
+    return self.variant(Valtype);
 }
 
-test "read rest of line" {
-    const source = "hello   my friend";
-    var scanner = @This().init(source);
-    _ = try scanner.nextWord();
-    try expect(std.mem.eql(u8, scanner.readUntil("\n"), "  my friend"));
+pub fn prefix(self: *@This()) ?Prefix {
+    return self.variant(Prefix);
 }
 
-test "newline" {
-    const source = "\n  ";
-    var scanner = @This().init(source);
-    try expect((try scanner.nextWord()).newline == 2);
+pub fn importExportType(self: *@This()) ?ImportExportType {
+    return self.variant(ImportExportType);
 }
 
-test "double newline" {
-    const source = "\n\n    ";
-    var scanner = @This().init(source);
-    try expect((try scanner.nextWord()).doubleNewline == 4);
+test "scan valtype" {
+    var scanner = @This().init("f64 i32 funcref");
+    try expect(scanner.valtype().? == .f64);
+    try expect(scanner.valtype().? == .i32);
+    try expect(scanner.valtype().? == .funcref);
 }
 
-test "tripple newline" {
-    const source = "\n\n\n    ";
-    var scanner = @This().init(source);
-    try expect(scanner.nextWord() == error.TooManyNewline);
+test "scan import export type" {
+    var scanner = @This().init("global func memory table");
+    try expect(scanner.importExportType().? == .global);
+    try expect(scanner.importExportType().? == .func);
+    try expect(scanner.importExportType().? == .memory);
+    try expect(scanner.importExportType().? == .table);
 }
 
-test "unused indent" {
-    const source = "\n  \n";
-    var scanner = @This().init(source);
-    try expect(scanner.nextWord() == error.UnusedIndentation);
+test "scan prefix" {
+    var scanner = @This().init("import func type global");
+    try expect(scanner.prefix() == .import);
+    try expect(scanner.prefix() == .func);
+    try expect(scanner.prefix() == .type);
+    try expect(scanner.prefix() == .global);
 }
 
-test "previous word" {
-    const source = "hello world\n\n  hello";
-    var scanner = @This().init(source);
-    const first_word = try scanner.nextWord();
-    const second_word = try scanner.nextWord();
-    const third_word = try scanner.nextWord();
-    const fourth_word = try scanner.nextWord();
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, fourth_word.word));
-    try expect((try scanner.previousWord()).doubleNewline == third_word.doubleNewline);
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, second_word.word));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, first_word.word));
+test "scan string" {
+    var scanner = @This().init(" Hi!\\n \n");
+    try expect(eql(u8, scanner.string().?, " Hi!\\n "));
 }
 
-test "func result thingie" {
-    const source = "func main param x i32 y i32 result i32 result i32 i32 i32";
-    var scanner = @This().init(source);
-    _ = scanner.readUntil("\n");
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "result"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "result"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "y"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "i32"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "x"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "param"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "main"));
-    try expect(std.mem.eql(u8, (try scanner.previousWord()).word, "func"));
-    try expect(try scanner.previousWord() == .beginningOfFile);
+test "scan word" {
+    var scanner = @This().init("1 2  3");
+    try expect(eql(u8, scanner.word().?, "1"));
+    try expect(eql(u8, scanner.word().?, "2"));
+    try expect(scanner.word() == null);
+    try expect(scanner.indentation() == 1);
+    try expect(eql(u8, scanner.word().?, "3"));
+}
+
+test "scan integer" {
+    var scanner = @This().init("1234567");
+    try expect(scanner.integer(i32).? == 1234567);
+}
+
+test "scan float" {
+    var scanner = @This().init("1.25");
+    try expect(scanner.float(f32).? == 1.25);
+}
+
+test "scan newline" {
+    var scanner = @This().init("\n  ");
+    try expect(scanner.newline());
+}
+
+test "indentation" {
+    var scanner = @This().init("    ");
+    try expect(scanner.indentation() == 4);
+}
+
+test "scan double newline" {
+    var scanner = @This().init("\n\n   ");
+    try expect(scanner.newline());
+    try expect(scanner.newline());
 }
