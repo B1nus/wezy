a328086bb50cd1e6103ea6c53558c5c035e56a53
B1nus <123@1>
2025-05-22 19:22:20 +0200

    Nicer ast

--- README.md
+++ README.md
@@ -1,100 +1,12 @@
 # we
 Inspired by [Scratch](https://scratch.mit.edu/), [Lua](https://www.lua.org/start.html), [LÖVE](https://www.love2d.org/), [Rust](https://www.rust-lang.org/) and [Zig](https://ziglang.org/). A great first language and a great next language to learn after Scratch. Head over to the [website](https://b1nus.github.io/crust/) to start coding. Have fun!
 
-# Entire Language
-```
-function raise_to_integer(x, y s64) _ # Decide the type for x and the return type at compile time
-    return x ^ y # Raise x to the power of y
+# Goals
+- Easy to iterate (learn from zig's mistakes) (hotreloading)
+- Super (almost too) simple building blocks
+- Beginner-friendly errors (simple terminology, short but clear errors with example solutions)
+- Automatic error correction (":" before types for example) ("loop","for","while" instead of "repeat")
 
-x, y s64 # Declare types
-x, y s64, _ s64, _ = 1, 2, 3, 4 # Assign values
-
-repeat 10 ^ 3
-    if true
-        break
-    else if false
-        continue
-
-use "wasi:io/streams" as wasi
-
-# Hmmm
-record list(item_type type)
-    pointer u32
-    length u32
-
-    item_byte_size = byte_size_of(item_type)
-    item_byte_alignment = byte_alignment_of(item_type)
-    
-    function append(this, item item_type)
-        this.pointer = reallocate(this.pointer, this.length * item_byte_size, item_byte_alignment, (this.length + 1) * item_byte_size)
-        this.length = this.length + 1
-
-record player
-    x f32
-    y f32
-    health health_bar
-    name [u8]
-
-    function move(this, dx, dy)
-        this.x = this.x + dx
-        this.y = this.y + dy
-
-    record health_bar
-        max u64
-        current u64
-
-player_instance = player
-    x 1.0
-    y 5.0
-    health player.health_bar
-        max 100
-        current 100
-    name "guy1"
-
-player.move(1, -1)
-player.name.append('!')
-
-variant result
-    contents [u8]
-    not_found
-    out_of_memory
-    generall
-
-file_read_result = result.contents "Sup homie, I see you readin that file."
-assert(file_read_result == .contents) # Shorthand for variant literal kind
-
-# Da fuck is this?
-function double_function(func function(_) _) function(_) _
-    function doubled(x) _
-        return func(x) * 2
-    
-    return doubled
-
-# Labels
-repeat as outer
-    repeat as inner
-        if true
-            break outer
-        else
-            continue inner
-
-hello_world = "Hello, world!\n"
-# Memory related methods
-pointer = hello_world.pointer
-length = hello_world.length
-
-# Memory functions
-byte = read_byte(pointer)
-assert(byte == 'H')
-write_byte(pointer, 'h')
-assert(hello_world == "hello, world!\n")
-# Don't forget about allocate, reallocate and free
-
-# Write out all builtin functionsand methods
-# Write out all builtin constants
-
-# Generics could be allowed by allowing types as values
-```
 Look [what people are able to do in geometry dash](https://www.youtube.com/watch?v=2I02aKeqgpM). It's not built for this, but creative people still find a way. I love this so much.
 # Simplicity Idea Nuggets
 - Functions implicitly return and reassign their arguments.
---
+++ compiler/ast.zig
@@ -0,0 +1,164 @@
+const std = @import("std");
+const List = std.ArrayList;
+
+const Block = List(Statement);
+
+const Statement = union(enum) {
+    function: Function,
+    @"return": List(Value),
+    @"continue": ?[]const u8,
+    @"break": ?[]const u8,
+    variant: Variant,
+    record: Record,
+    repeat: Repeat,
+    assign: Assign,
+    bind: Bind,
+    use: Use,
+    @"if": If,
+};
+
+const Function = struct {
+    name: []const u8,
+    parameters: List([]const u8),
+    type: FunctionType,
+    body: Block,
+};
+
+const Use = struct {
+    path: []const u8,
+    alias: ?[]const u8,
+};
+
+const Repeat = struct {
+    label: ?[]const u8,
+    value: ?Value, // Allow booleans for while loops?
+    body: Block,
+};
+
+const If = struct {
+    condition: Value,
+    success: Block,
+    failure: Block,
+};
+
+const Bind = struct {
+    names: List([]const u8),
+    type: Type,
+};
+
+const Assign = struct {
+    names: List(Name),
+    types: List(Type),
+    values: List(Value),
+};
+
+const Record = struct {
+    name: []const u8,
+    fields: List([]const u8),
+    values: List(Type),
+};
+
+const Variant = struct {
+    name: []const u8,
+    fields: List([]const u8),
+    values: List(?Type),
+};
+
+const Name = struct {
+    name: []const u8,
+    field: List([]const u8),
+};
+
+const ListValue = List(Value);
+const SetValue = List(Value);
+const MapValue = List([2]Value);
+const VariantValue = struct {
+    variant: Name,
+    field: []const u8,
+    value: *Value,
+};
+const RecordValue = struct {
+    record: Name,
+    fields: List([]const u8),
+    values: List(Value),
+};
+const BinaryValue = [2]*Value;
+const UnaryValue = *Value;
+
+pub const Value = union(enum) {
+    integer: u64,
+    float: f64,
+    string: List(u8),
+    char: u32,
+    bool: bool,
+    @"and": BinaryValue,
+    @"or": BinaryValue,
+    @"xor": BinaryValue,
+    not: UnaryValue,
+    negative: UnaryValue,
+    positive: UnaryValue,
+    addition: BinaryValue,
+    subtraction: BinaryValue,
+    multiplication: BinaryValue,
+    division: BinaryValue,
+    exponentiation: BinaryValue,
+    remainder: BinaryValue,
+    shift_left: BinaryValue,
+    shift_right: BinaryValue,
+    list: ListValue,
+    set: SetValue,
+    map: MapValue,
+    name: Name,
+    variant: VariantValue,
+    record: RecordValue,
+};
+
+const ListType = *Type;
+const SetType = *Type;
+const MapType = [2]*Type;
+const FunctionType = struct {
+    this: ?[]const u8,
+    parameters: List(Type),
+    result: List(Type),
+};
+
+const Type = union(enum) {
+    s8,
+    s16,
+    s32,
+    s64,
+    u8,
+    u16,
+    u32,
+    u64,
+    bool,
+    set: SetType,
+    map: MapType,
+    list: ListType,
+    name: Name,
+    function: FunctionType,
+};
+
+test {
+    const allocator = std.heap.c_allocator; // I don't care about leaks. fuck of
+    var block = Block.init(allocator);
+
+    var function = Function {
+        .name = "do_thing",
+        .parameters = List([]const u8).init(allocator),
+        .type = FunctionType {
+            .this = null,
+            .parameters = List(Type).init(allocator),
+            .result = List(Type).init(allocator),
+        },
+        .body = List(Statement).init(allocator),
+    };
+
+    const item_type = try allocator.create(Type);
+
+    const list = Type { .list = item_type };
+    try function.type.parameters.append(list);
+    try function.type.parameters.append(Type { .function = function.type });
+
+    try block.append(Statement { .function = function });
+}
